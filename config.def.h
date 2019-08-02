47f2d7e1ed50a748426ae35d45d2c81fd441cff9
keni7385 <andrea.corsini@outlook.com>
2019-08-02 12:38:06 +0200

    Added solarized theme colors

--- config.def.h
+++ config.def.h
@@ -7,15 +7,18 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+
+static const char col_norm_border[] = "#073642";
+static const char col_norm_bg[]     = "#002B36";
+static const char col_norm_fg[]     = "#839496";
+static const char col_sele_border[]  = "#586E75";
+static const char col_sele_bg[]      = "#073642";
+static const char col_sele_fg[]      = "#839496";
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*               fg           bg           border   */
+	[SchemeNorm] = { col_norm_fg, col_norm_bg, col_norm_border },
+	[SchemeSel]  = { col_sele_fg, col_sele_bg, col_sele_border },
 };
 
 /* tagging */
@@ -56,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_norm_bg, "-nf", col_norm_fg, "-sb", col_sele_bg, "-sf", col_sele_fg, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *passmenucmd[] = {"passmenu", "--type", NULL};
 static const char *dmenu_netctlcmd[] = {"dmenu_netctl", NULL};
